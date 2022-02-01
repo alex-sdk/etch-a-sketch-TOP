9a34a0326e42f24e16b55a6828a0c2b9f21fbbc7
Cev Dev <alex-sdk@users.noreply.github.com>
2022-02-01 16:17:51 -0500

    User input grid size

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-function makeGrid() {
-    let width = 32;
-    let height = 32;
+function makeGrid(height=50, width=50) {
+    let squareSizeHeight = 700 / height -2;
+    let squareSizeWidth = 700 / width - 2;
     
     const container = document.querySelector('.container')
+    container.replaceChildren();
     for (let i = 0; i < width; i++){
         const column = document.createElement('div')
         column.className = 'column';
         for (let j = 1; j <= height; j++){
             let cell = document.createElement('div');
             cell.className = 'gridsquare';
+            cell.style.height = `${squareSizeHeight}px`
+            cell.style.width = `${squareSizeWidth}px`
             column.appendChild(cell)
         }
         container.appendChild(column);
@@ -22,8 +25,19 @@ function changeColor() {
         });
     });
 }
-function clearCanvas() {
-    
+function resetCanvas() {
+    let width = Number(prompt("How many squares in width?"));
+    let height = Number(prompt("How many squares in height?"));
+    if (width > 100 || height > 100) {
+        return alert("Please enter numbers only no bigger than 100.");
+    }
+    makeGrid(height, width)
+    changeColor()
 }
-makeGrid()
-changeColor()
+function etchASketch() {
+    makeGrid()
+    changeColor()
+    let btn = document.querySelector(".reset");
+    btn.addEventListener("click", resetCanvas); 
+}
+etchASketch()
